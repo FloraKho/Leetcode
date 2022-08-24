f7b9097a65211b47222637cbb5a5ea9ab84d362c
Flora Xu <91162637+FloraKho@users.noreply.github.com>
2022-08-23 19:03:39 -0700

    Time: 71 ms (60.40%), Space: 14.4 MB (67.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,11 +2,8 @@ import collections
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
-        print(sorted_s)
-        print(sorted_t)
-        
-        if collections.Counter(sorted_s) == collections.Counter(sorted_t):
+        # sorted_s = sorted(s)
+        # sorted_t = sorted(t)
+        if collections.Counter(s) == collections.Counter(t):
             return True
         return False
