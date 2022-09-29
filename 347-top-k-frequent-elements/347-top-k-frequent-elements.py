bef33dc31bbec85c6775c1fbf45c7f8aad28c818
Flora Xu <91162637+FloraKho@users.noreply.github.com>
2022-09-29 16:17:03 -0700

    Time: 111 ms (91.92%), Space: 18.8 MB (47.95%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,5 +1,20 @@
-from collections import Counter
 class Solution:
 	def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-		count = collections.Counter(nums)
-		return heapq.nlargest(k,count.keys(),key = count.get)
+
+		frequency = {}
+
+		for num in nums:
+
+			if num not in frequency:
+
+				frequency[num] = 1
+
+			else:
+
+				frequency[num] = frequency[num] + 1
+
+		frequency = dict(sorted(frequency.items(), key=lambda x: x[1], reverse=True))
+
+		result = list(frequency.keys())[:k]
+
+		return result
