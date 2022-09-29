22c7e9dc37ce8c9acc3fe6ef29b3aa2210616fe4
Flora Xu <91162637+FloraKho@users.noreply.github.com>
2022-09-29 16:35:12 -0700

    Time: 105 ms (93.43%), Space: 17 MB (96.74%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,8 +2,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         charCount = {}
         for string in strs:
-            sorted_string = ''.join(sorted(string)) #'aet'
+            sorted_string = ''.join(sorted(string)) #'aet', 'ant', 'abt'
             if sorted_string not in charCount:
-                charCount[sorted_string] = []
-            charCount[sorted_string].append(string)
-        return list(charCount.values())
+                charCount[sorted_string] = [] # charCount = {'aet': [], 'ant': [], 'abt': []}
+            charCount[sorted_string].append(string) #charCount = {'aet': ['eat', 'tea', 'ate'], 'ant': ['tan', 'nat'], 'abt': ['bat']}
+        return list(charCount.values()) #charCount.values() -> each list
