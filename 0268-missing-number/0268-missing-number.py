c79e9ea95fc6716bcf9b243fdb21a33228e75423
Flora Xu <91162637+FloraKho@users.noreply.github.com>
2022-10-11 11:16:42 -0700

    Time: 274 ms (52.94%), Space: 15.7 MB (10.97%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        #a list of numbers
+        #the end of range is the length of numbers
+        #find the missing number within the range
+        #using set() method to convert with disctinct elements
+        #using for...in... method
+        #if the number is not in set
+        #return missing number
+        
+        num_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in num_set:
+                return number
