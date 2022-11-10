91cd79168fac9f409e90d7e5feed1ca3521dfe89
Flora Xu <91162637+FloraKho@users.noreply.github.com>
2022-11-09 18:18:33 -0800

    Time: 114 ms (21.85%), Space: 13.9 MB (30.43%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        pair_value = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        
+        ans = 0
+        
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        
+        for char in s:
+            ans += pair_value[char]
+        return ans
